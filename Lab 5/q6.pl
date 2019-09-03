8ec62615c0f118f5d9500562e3d8e41fa503ec55
Noah King <nki38@uclive.ac.nz>
2019-09-03 12:21:09 +1200

    Update for q6 only

--- Lab 5/q6.pl
+++ Lab 5/q6.pl
@@ -6,36 +6,40 @@
 
 
 
-%Note: ?- append([A|B],C,D).
-          % B = [],
-          % D = [A|C] .
 
-%
-% %per usual, define a base case for our recursive definition:
-swap_ends([], []).
-swap_ends(L1, L2) :-
-	append([H1 | B1], [H2], L1),
-	append([H2 | B1], [H1], L2).
+%We need to ensure that the input list has at least two elements
+%We shall use an auxiliary predicate to require this:
+swap_ends([X1, X2| Xs], [Y1, Y2| Ys]) :-
+												swap_ends_2plus([X1, X2| Xs], [Y1, Y2| Ys]).
 
 
-%NOTE: This does allow input list < 2 in size. Need to fix,
+
+% note that Xs may be blank, but we must unify with  the left of each pred arg
 
 
-% test_answer :-
-%     swap_ends([a, b, c, d, e, f], L),
-%     writeln(L).
-%
-% test_answer :-
-%     swap_ends(L1, L2),
-%     writeln('OK').
-%
-% test_answer :-
-%     swap_ends(L, [term1, term2, term3, term4]),
-%     writeln(L).
-%
-% test_answer :-
-%     swap_ends([367], L),
-%     writeln('Wrong answer!').
-%
-% test_answer :-
-%     writeln('OK').
+swap_ends_2plus([], []).
+swap_ends_2plus(L1, L2) :-
+												append([H1| B1], [H2], L1),
+	    									append([H2| B1], [H1], L2).
+
+
+
+
+test_answer :-
+    swap_ends([a, b, c, d, e, f], L),
+    writeln(L).
+
+test_answer :-
+    swap_ends(L1, L2),
+    writeln('OK').
+
+test_answer :-
+    swap_ends(L, [term1, term2, term3, term4]),
+    writeln(L).
+
+test_answer :-
+    swap_ends([367], L),
+    writeln('Wrong answer!').
+
+test_answer :-
+    writeln('OK').
